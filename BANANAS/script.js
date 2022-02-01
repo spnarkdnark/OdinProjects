f6c81505e777e4cef39e5b2bebe9683b0459d6b8
Spnarkdnark <samuelfudala@gmail.com>
2022-01-31 21:04:07 -0500

    background stylin

---
+++ BANANAS/script.js
@@ -0,0 +1,8 @@
+const leftBanana = document.querySelector("#left")
+const centerBanana = document.querySelector("#center");
+const rightBanana = document.querySelector("#right");
+
+leftBanana.addEventListener('click',function(){console.log('left')});
+centerBanana.addEventListener('click',function(){console.log('center')});
+rightBanana.addEventListener('click',function(){console.log('right')});
+
