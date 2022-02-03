5ccfee2ca365e39e56c47e4b6d413e14c1af7a03
Spnarkdnark <samuelfudala@gmail.com>
2022-02-03 10:49:39 -0500

    grid built and styled, mouseoverworks

--- EFF_SKETCH/script.js
+++ EFF_SKETCH/script.js
@@ -1,16 +1,14 @@
-let gridValue = 16;
+let gridValue = 8;
+
 let sketchContainer = document.querySelector('.sketchContainer');
+
 let gridRow = document.createElement('div');
 gridRow.classList.add('gridRow');
+
 let gridUnit = document.createElement('div');
 gridUnit.classList.add('gridUnit');
 
 function drawGrid(){
-    gridUnitHeight = sketchContainer.offsetWidth / gridValue;
-    gridUnitWidth = gridUnit.height;
-    gridRowWidth = sketchContainer.offsetWidth;
-    gridRowHeight = gridUnit.height;
-  
 
     for (i = 0; i < gridValue; i++){
         tempGridRow = document.createElement('div');
@@ -27,4 +25,42 @@ function drawGrid(){
 
 }
 
-drawGrid();
+function getUnitDimension(gridValue){
+    return sketchContainer.offsetHeight / gridValue;
+}
+
+function styleGrid(gridValue){
+    let unitDim = getUnitDimension(gridValue);
+    let unitDimConvert = unitDim.toString();
+
+    let allUnits = document.querySelectorAll('.gridUnit');
+
+    for (i = 0; i < allUnits.length; i++){
+        allUnits[i].style.height = `${unitDimConvert}px`;
+        allUnits[i].style.width = `${unitDimConvert}px`;
+        
+    }
+}
+
+function changeColor(){
+    this.style.backgroundColor = 'black';
+    return false;
+}
+
+function addGridListeners(){
+    let allUnits = document.querySelectorAll('.gridUnit');
+    
+    for (i = 0; i < allUnits.length; i++){
+        allUnits[i].addEventListener('mouseover', changeColor);
+    }
+}
+
+
+function buildGrid(gridValue){
+    drawGrid();
+    styleGrid(gridValue);
+    addGridListeners();
+}
+
+
+buildGrid(gridValue);
