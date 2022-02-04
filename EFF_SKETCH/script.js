0930bdbc9348b253efeb346f7e2e5cd2a3d26908
Spnarkdnark <samuelfudala@gmail.com>
2022-02-03 19:22:34 -0500

    ready to crack down on the shirt canvas

--- EFF_SKETCH/script.js
+++ EFF_SKETCH/script.js
@@ -1,15 +1,31 @@
-let gridValue = 16;
 
 let sketchContainer = document.querySelector('.sketchContainer');
-
+gridCount = 16;
 let gridRow = document.createElement('div');
 gridRow.classList.add('gridRow');
 
 let gridUnit = document.createElement('div');
 gridUnit.classList.add('gridUnit');
 
-function drawGrid(){
+let eightButton = document.getElementById('eight');
+let sixteenButton = document.getElementById('sixteen');
+let sixtyFourButton = document.getElementById('sixtyfour');
 
+eightButton.addEventListener('click', function(){refreshGrid(8)});
+sixteenButton.addEventListener('click', function(){refreshGrid(16)});
+sixtyFourButton.addEventListener('click', function(){refreshGrid(64)});
+
+let mouseDown = false;
+
+sketchContainer.onmousedown = function() {
+    mouseDown = true;
+}
+
+sketchContainer.onmouseup = function() {
+    mouseDown = false;
+}
+
+function drawGrid(gridValue){
     for (i = 0; i < gridValue; i++){
         tempGridRow = document.createElement('div');
         tempGridRow.id = i;
@@ -23,6 +39,20 @@ function drawGrid(){
         sketchContainer.appendChild(tempGridRow);
     }
 
+}
+
+function killGrid(container){
+    while (container.firstElementChild){
+        container.firstElementChild.remove();
+    }
+}
+
+let trashIcon = document.querySelector('.trashIcon');
+trashIcon.addEventListener('click', trashClick);
+
+function trashClick(){
+    killGrid(sketchContainer);
+    buildGrid(gridCount);
 }
 
 function getUnitDimension(gridValue){
@@ -43,8 +73,13 @@ function styleGrid(gridValue){
 }
 
 function changeColor(){
-    this.style.backgroundColor = 'black';
-    return false;
+    if (mouseDown){
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function changeColorBrute(){
+        this.style.backgroundColor = 'black';
 }
 
 function addGridListeners(){
@@ -52,15 +87,21 @@ function addGridListeners(){
     
     for (i = 0; i < allUnits.length; i++){
         allUnits[i].addEventListener('mouseover', changeColor);
+        allUnits[i].addEventListener('mousedown', changeColorBrute);
     }
 }
 
 
 function buildGrid(gridValue){
-    drawGrid();
+    drawGrid(gridValue);
     styleGrid(gridValue);
     addGridListeners();
 }
 
+function refreshGrid(gridValue){
+    killGrid(sketchContainer);
+    buildGrid(gridValue);
+    gridCount = gridValue;
+}
 
-buildGrid(gridValue);
+buildGrid(gridCount);
